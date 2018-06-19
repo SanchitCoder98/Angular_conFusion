2ecaaa5c414286002d02db359bfd1d43542f8a96
Sanchit Vohra <vohrasanchit@gmail.com>
2018-06-19 20:39:00 +0530

    assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,7 @@ import { PromotionService } from './services/promotion.service';
     AngularFontAwesomeModule,
     AppRoutingModule
   ],
-  providers: [ DishService, PromotionService ],
+  providers: [ DishService, PromotionService, LeaderService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
