e534ec805a3bc0bd258fa8ac0e78888e2d559b61
Sanchit Vohra <vohrasanchit@gmail.com>
2018-06-27 12:56:01 +0530

    assignment 3'

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/feedback';
 
 @Component({
   selector: 'app-dishdetail',
@@ -12,19 +16,86 @@ import { switchMap } from 'rxjs/operators';
 })
 export class DishdetailComponent implements OnInit {
 
+  comment: Comment;
+  feedbackForm: FormGroup;
   dish: Dish;
   dishIds: number[];
   prev: number;
   next: number;
   
+  formErrors = {
+    'name': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'name': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.'
+    },
+    'comment': {
+      'required':      'Comment is required.',
+    },
+  };
+
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private fb: FormBuilder) { 
+      this.createForm();
+    }
 
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(+params['id'])))
     .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+
+  createForm()
+  {
+    this.feedbackForm = this.fb.group({
+      name:['', [Validators.required, Validators.minLength(2)]],
+      rating:'5',
+      comment:['', [Validators.required]]
+    });
+
+    this.feedbackForm.valueChanges
+    .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
+  }
+
+  onSubmit() {
+    //this.feedback = this.feedbackForm.value;
+    this.comment = {
+      rating: this.feedbackForm.value.rating,
+      comment: this.feedbackForm.value.comment,
+      author: this.feedbackForm.value.name,
+      date: new Date().toISOString()
+    }
+    this.dish.comments.push(this.comment);
+    console.log(this.comment);
+    this.feedbackForm.reset({
+      name:'',
+      rating:'5',
+      comment:''
+    });
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
   }
 
   goBack(): void {
